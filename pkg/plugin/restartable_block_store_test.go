c77c17c8807f3ffa08024e63ebb17e30e5d51e1e
Andy Goldstein <andy.goldstein@gmail.com>
2018-06-20 15:40:33 -0400

    add restartable block store test
    Signed-off-by: Andy Goldstein <andy.goldstein@gmail.com>

---
+++ pkg/plugin/restartable_block_store_test.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2018 the Heptio Ark contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugin
+
+import (
+	"testing"
+
+	"github.com/heptio/ark/pkg/cloudprovider/mocks"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRestartableGetBlockStore(t *testing.T) {
+	tests := []struct {
+		name          string
+		plugin        interface{}
+		getError      error
+		expectedError string
+	}{
+		{
+			name:          "error getting by kind and name",
+			getError:      errors.Errorf("get error"),
+			expectedError: "get error",
+		},
+		{
+			name:          "wrong type",
+			plugin:        3,
+			expectedError: "int is not a cloudprovider.BlockStore!",
+		},
+		{
+			name:   "happy path",
+			plugin: new(mocks.BlockStore),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			p := new(mockRestartableProcess)
+			defer p.AssertExpectations(t)
+
+			name := "aws"
+			key := kindAndName{kind: PluginKindBlockStore, name: name}
+			p.On("getByKindAndName", key).Return(tc.plugin, tc.getError)
+			p.On("addReinitializer", key, mock.AnythingOfType("&restartableBlockStore"))
+
+			r := newRestartableBlockStore(name, p)
+			a, err := r.getBlockStore()
+			if tc.expectedError != "" {
+				assert.EqualError(t, err, tc.expectedError)
+				return
+			}
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.plugin, a)
+		})
+	}
+}
+
+func TestRestartableBlockStoreGetDelegate(t *testing.T) {
+	p := new(mockRestartableProcess)
+	defer p.AssertExpectations(t)
+
+	// Reset error
+	p.On("resetIfNeeded").Return(errors.Errorf("reset error")).Once()
+	name := "aws"
+	r := newRestartableBlockStore(name, p)
+	a, err := r.getDelegate()
+	assert.Nil(t, a)
+	assert.EqualError(t, err, "reset error")
+
+	// Happy path
+	p.On("resetIfNeeded").Return(nil)
+	expected := new(mocks.BlockStore)
+	key := kindAndName{kind: PluginKindBlockStore, name: name}
+	p.On("getByKindAndName", key).Return(expected, nil)
+
+	a, err = r.getDelegate()
+	assert.NoError(t, err)
+	assert.Equal(t, expected, a)
+}
