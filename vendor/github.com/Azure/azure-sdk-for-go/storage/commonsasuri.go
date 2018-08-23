a440029c2fb0486ca8a58e5b1ba9ebfc51b8b61f
Steve Kriss <steve@heptio.com>
2018-08-23 16:12:18 -0700

    bump Azure SDK version and include storage mgmt package
    Signed-off-by: Steve Kriss <steve@heptio.com>

--- vendor/github.com/Azure/azure-sdk-for-go/storage/commonsasuri.go
+++ vendor/github.com/Azure/azure-sdk-for-go/storage/commonsasuri.go
@@ -14,6 +14,25 @@ package storage
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-var (
-	sdkVersion = "v11.3.0-beta"
+import (
+	"net/url"
+	"time"
 )
+
+// SASOptions includes options used by SAS URIs for different
+// services and resources.
+type SASOptions struct {
+	APIVersion string
+	Start      time.Time
+	Expiry     time.Time
+	IP         string
+	UseHTTPS   bool
+	Identifier string
+}
+
+func addQueryParameter(query url.Values, key, value string) url.Values {
+	if value != "" {
+		query.Add(key, value)
+	}
+	return query
+}
--- vendor/github.com/Azure/azure-sdk-for-go/storage/file.go
+++ vendor/github.com/Azure/azure-sdk-for-go/storage/file.go
@@ -1,5 +1,19 @@
 package storage
 
+// Copyright 2017 Microsoft Corporation
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 import (
 	"errors"
 	"fmt"
@@ -426,7 +440,7 @@ func (f *File) URL() string {
 	return f.fsc.client.getEndpoint(fileServiceName, f.buildPath(), nil)
 }
 
-// WriteRangeOptions includes opptions for a write file range operation
+// WriteRangeOptions includes options for a write file range operation
 type WriteRangeOptions struct {
 	Timeout    uint
 	ContentMD5 string
