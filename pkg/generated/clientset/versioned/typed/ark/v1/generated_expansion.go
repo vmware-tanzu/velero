ZhangZhenhua/ark

Add VolumeLocation and Snapshot.
Note: still need to add docs.

pkg/generated/clientset/versioned/typed/ark/v1/generated_expansion.go
@@ -37,3 +37,7 @@ type ResticRepositoryExpansion interface{}
 type RestoreExpansion interface{}
 
 type ScheduleExpansion interface{}
+
+type SnapshotExpansion interface{}
+
+type VolumeLocationExpansion interface{}

pkg/generated/informers/externalversions/generic.go
@@ -73,6 +73,10 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().Restores().Informer()}, nil
 	case v1.SchemeGroupVersion.WithResource("schedules"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().Schedules().Informer()}, nil
+	case v1.SchemeGroupVersion.WithResource("snapshots"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().Snapshots().Informer()}, nil
+	case v1.SchemeGroupVersion.WithResource("volumelocations"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().VolumeLocations().Informer()}, nil
 
 	}
 

vmware-tanzu/velero

Signed-off-by: Carlisia <carlisia@grokkingtech.io>

pkg/generated/clientset/versioned/typed/ark/v1/generated_expansion.go
@@ -37,3 +37,7 @@ type ResticRepositoryExpansion interface{}
 type RestoreExpansion interface{}
 
 type ScheduleExpansion interface{}
+
+type VolumeSnapshotExpansion interface{}
+
+type VolumeSnapshotLocationExpansion interface{}

pkg/generated/informers/externalversions/generic.go
@@ -73,6 +73,10 @@ func (f *sharedInformerFactory) ForResource(resource schema.GroupVersionResource
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().Restores().Informer()}, nil
 	case v1.SchemeGroupVersion.WithResource("schedules"):
 		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().Schedules().Informer()}, nil
+	case v1.SchemeGroupVersion.WithResource("volumesnapshots"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().VolumeSnapshots().Informer()}, nil
+	case v1.SchemeGroupVersion.WithResource("volumesnapshotlocations"):
+		return &genericInformer{resource: resource.GroupResource(), informer: f.Ark().V1().VolumeSnapshotLocations().Informer()}, nil
 
 	}
 