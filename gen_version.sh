2fdcecdf4914b680d170269ac198013d82763dd3
Nolan Brubaker <brubakern@vmware.com>
2019-02-20 15:49:23 -0500

    Use local git checkout to build docs
    This change enables us to build documentation prior to actually cutting
    a tag.
    
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- gen_version.sh
+++ gen_version.sh
@@ -6,11 +6,13 @@ fi
 
 VERSION=$1
 
+# If the version is master, we'll re-use the dir.
 if [[ $VERSION != "master" ]]; then
-  GH_BASE_URL=https://github.com/heptio/velero/tags/$VERSION
-else
-  GH_BASE_URL=https://github.com/heptio/velero/branches/master
+  mkdir $VERSION
 fi
 
-svn export $GH_BASE_URL/docs/ $VERSION/ || (echo "Failed to copy docs for version $VERSION" && exit -1)
-svn export --force $GH_BASE_URL/README.md $VERSION/README.md || (echo "Failed to copy README for version $VERSION" && exit -1)
+# Get the documentation out of the local clone of master and place them into the $VERSION's directory
+# --strip-components 1 makes sure we don't end up with docs in $VERSIONS/docs
+git archive master docs/ | tar -x --strip-components 1 -C $VERSION
+# The README is used as the index
+git show master:README.md > $VERSION/README.md
