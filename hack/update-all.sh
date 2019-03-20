a111eed2af8c0bb37ecd6d682b3faad116fbad2f
Steve Kriss <stephen.kriss@gmail.com>
2019-03-20 13:32:48 -0600

    update license headers to Velero contributors (#1302)
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- hack/update-all.sh
+++ hack/update-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2017 the Heptio Ark contributors.
+# Copyright 2017 the Velero contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- pkg/plugin/clientmgmt/process.go
+++ pkg/plugin/clientmgmt/process.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 the Heptio Ark contributors.
+Copyright 2018 the Velero contributors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
