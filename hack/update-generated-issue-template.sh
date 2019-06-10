0d028bb2ca8365109006e4fcb32035008251a74b
Steve Kriss <krisss@vmware.com>
2019-06-10 10:43:20 -0600

    move issue-template-gen from docs/ to hack/
    Signed-off-by: Steve Kriss <krisss@vmware.com>

--- hack/update-generated-issue-template.sh
+++ hack/update-generated-issue-template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #
-# Copyright 2018 the Velero contributors.
+# Copyright 2018, 2019 the Velero contributors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ BIN=${VELERO_ROOT}/_output/bin
 mkdir -p ${BIN}
 
 echo "Updating generated Github issue template"
-go build -o ${BIN}/issue-tmpl-gen ./docs/issue-template-gen/main.go
+go build -o ${BIN}/issue-tmpl-gen ./hack/issue-template-gen/main.go
 
 if [[ $# -gt 1 ]]; then
   echo "usage: ${BASH_SOURCE} [OUTPUT_FILE]"
