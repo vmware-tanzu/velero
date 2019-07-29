023e291fc59720fbdf3c8065883c63cdc26bc6e7
Steve Kriss <krisss@vmware.com>
2019-07-29 16:42:51 -0600

    add pod builder
    Signed-off-by: Steve Kriss <krisss@vmware.com>

---
+++ pkg/builder/pod_builder.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2019 the Velero contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package builder
+
+import (
+	corev1api "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// PodBuilder builds Pod objects.
+type PodBuilder struct {
+	object *corev1api.Pod
+}
+
+// ForPod is the constructor for a PodBuilder.
+func ForPod(name string) *PodBuilder {
+	return &PodBuilder{
+		object: &corev1api.Pod{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: corev1api.SchemeGroupVersion.String(),
+				Kind:       "Pod",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+		},
+	}
+}
+
+// Result returns the built Pod.
+func (b *PodBuilder) Result() *corev1api.Pod {
+	return b.object
+}
+
+// ObjectMeta applies functional options to the Pod's ObjectMeta.
+func (b *PodBuilder) ObjectMeta(opts ...ObjectMetaOpt) *PodBuilder {
+	for _, opt := range opts {
+		opt(b.object)
+	}
+
+	return b
+}
+
+// Volumes appends to the pod's volumes
+func (b *PodBuilder) Volumes(volumes ...corev1api.Volume) *PodBuilder {
+	b.object.Spec.Volumes = append(b.object.Spec.Volumes, volumes...)
+	return b
+}
