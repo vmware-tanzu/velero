2684f3eda0028399b736b5c90bdda8a7b033332a
Carlisia <carlisia@vmware.com>
2019-08-27 12:31:17 -0700

    Better godoc
    Signed-off-by: Carlisia <carlisia@vmware.com>

--- pkg/restore/restore_test.go
+++ pkg/restore/restore_test.go
@@ -2119,7 +2119,8 @@ func (f *fakeResticRestorerFactory) NewRestorer(ctx.Context, *velerov1api.Restor
 	return f.restorer, nil
 }
 
-// TestRestoreWithRestic runs restores of only pods that contain associated PVBs.
+// TestRestoreWithRestic verifies that a call to Restore was made as and when
+// expected for the given pods by using a mock for the restic restorer.
 func TestRestoreWithRestic(t *testing.T) {
 	tests := []struct {
 		name                        string
