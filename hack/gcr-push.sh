6e3209e6e3af7980798b1cf611b4b82af36551f1
Nolan Brubaker <brubakern@vmware.com>
2019-08-27 15:57:43 -0400

    Explain the latest tag logic
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- hack/gcr-push.sh
+++ hack/gcr-push.sh
@@ -3,8 +3,14 @@
 # Return value is written into LATEST
 LATEST=""
 function latest_release() {
-# Loop through the tags since pre-release versions come before the actual versions.
-# Iterate til we find the first non-pre-release
+    # Loop through the tags since pre-release versions come before the actual versions.
+    # Iterate til we find the first non-pre-release
+
+    # This is not necessarily the most recently made tag; instead, we want it to be the highest semantic version.
+    # The most recent tag could potentially be a lower semantic version, made as a point release for a previous series.
+    # As an example, if v1.3.0 exists and we create v1.2.2, v1.3.0 should still be `latest`.
+    # `git describe --tags $(git rev-list --tags --max-count=1)` would return the most recently made tag.
+
     for t in $(git tag -l --sort=-v:refname);
     do
         # If the tag has alpha, beta or rc in it, it's not "latest"
