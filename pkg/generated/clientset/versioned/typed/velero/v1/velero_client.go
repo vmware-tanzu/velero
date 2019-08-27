772459a8b40fd3d26e98397e27302d235b4d1584
Adnan Abdulhussein <aadnan@vmware.com>
2019-08-27 12:00:04 -0700

    generated: code-generator updates
    Signed-off-by: Adnan Abdulhussein <aadnan@vmware.com>

--- pkg/generated/clientset/versioned/typed/velero/v1/velero_client.go
+++ pkg/generated/clientset/versioned/typed/velero/v1/velero_client.go
@@ -21,7 +21,6 @@ package v1
 import (
 	v1 "github.com/heptio/velero/pkg/apis/velero/v1"
 	"github.com/heptio/velero/pkg/generated/clientset/versioned/scheme"
-	serializer "k8s.io/apimachinery/pkg/runtime/serializer"
 	rest "k8s.io/client-go/rest"
 )
 
@@ -121,7 +120,7 @@ func setConfigDefaults(config *rest.Config) error {
 	gv := v1.SchemeGroupVersion
 	config.GroupVersion = &gv
 	config.APIPath = "/apis"
-	config.NegotiatedSerializer = serializer.DirectCodecFactory{CodecFactory: scheme.Codecs}
+	config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
 
 	if config.UserAgent == "" {
 		config.UserAgent = rest.DefaultKubernetesUserAgent()
