45d3aabc0a0d324b540997ec137f52ae68c0e4c5
Steven Chung <schung@d2iq.com>
2019-10-03 16:14:28 -0400

    handle self-signed certs with insecureSkipTLSVerify server config
    Signed-off-by: Steven Chung <schung@d2iq.com>

--- pkg/cloudprovider/aws/object_store.go
+++ pkg/cloudprovider/aws/object_store.go
@@ -17,7 +17,9 @@ limitations under the License.
 package aws
 
 import (
+	"crypto/tls"
 	"io"
+	"net/http"
 	"sort"
 	"strconv"
 	"time"
@@ -36,14 +38,15 @@ import (
 )
 
 const (
-	s3URLKey                = "s3Url"
-	publicURLKey            = "publicUrl"
-	kmsKeyIDKey             = "kmsKeyId"
-	s3ForcePathStyleKey     = "s3ForcePathStyle"
-	bucketKey               = "bucket"
-	signatureVersionKey     = "signatureVersion"
-	credentialProfileKey    = "profile"
-	serverSideEncryptionKey = "serverSideEncryption"
+	s3URLKey                 = "s3Url"
+	publicURLKey             = "publicUrl"
+	kmsKeyIDKey              = "kmsKeyId"
+	s3ForcePathStyleKey      = "s3ForcePathStyle"
+	bucketKey                = "bucket"
+	signatureVersionKey      = "signatureVersion"
+	credentialProfileKey     = "profile"
+	serverSideEncryptionKey  = "serverSideEncryption"
+	insecureSkipTLSVerifyKey = "insecureSkipTLSVerify"
 )
 
 type s3Interface interface {
@@ -86,27 +89,30 @@ func (o *ObjectStore) Init(config map[string]string) error {
 		signatureVersionKey,
 		credentialProfileKey,
 		serverSideEncryptionKey,
+		insecureSkipTLSVerifyKey,
 	); err != nil {
 		return err
 	}
 
 	var (
-		region               = config[regionKey]
-		s3URL                = config[s3URLKey]
-		publicURL            = config[publicURLKey]
-		kmsKeyID             = config[kmsKeyIDKey]
-		s3ForcePathStyleVal  = config[s3ForcePathStyleKey]
-		signatureVersion     = config[signatureVersionKey]
-		credentialProfile    = config[credentialProfileKey]
-		serverSideEncryption = config[serverSideEncryptionKey]
+		region                   = config[regionKey]
+		s3URL                    = config[s3URLKey]
+		publicURL                = config[publicURLKey]
+		kmsKeyID                 = config[kmsKeyIDKey]
+		s3ForcePathStyleVal      = config[s3ForcePathStyleKey]
+		signatureVersion         = config[signatureVersionKey]
+		credentialProfile        = config[credentialProfileKey]
+		serverSideEncryption     = config[serverSideEncryptionKey]
+		insecureSkipTLSVerifyVal = config[insecureSkipTLSVerifyKey]
 
 		// note that bucket is automatically added to the config map
 		// by the server from the ObjectStorageProviderConfig so
 		// doesn't need to be explicitly set by the user within
 		// config.
-		bucket           = config[bucketKey]
-		s3ForcePathStyle bool
-		err              error
+		bucket                = config[bucketKey]
+		s3ForcePathStyle      bool
+		insecureSkipTLSVerify bool
+		err                   error
 	)
 
 	if s3ForcePathStyleVal != "" {
@@ -129,6 +135,20 @@ func (o *ObjectStore) Init(config map[string]string) error {
 	serverConfig, err := newAWSConfig(s3URL, region, s3ForcePathStyle)
 	if err != nil {
 		return err
+	}
+
+	if insecureSkipTLSVerifyVal != "" {
+		if insecureSkipTLSVerify, err = strconv.ParseBool(insecureSkipTLSVerifyVal); err != nil {
+			return errors.Wrapf(err, "could not parse %s (expected bool)", insecureSkipTLSVerifyKey)
+		}
+	}
+
+	if insecureSkipTLSVerify {
+		serverConfig.HTTPClient = &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
 	}
 
 	serverSession, err := getSession(serverConfig, credentialProfile)
