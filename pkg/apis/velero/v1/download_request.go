f74de3fd3c657657e51fb0fe9c8e3dae17ef70da
Nolan Brubaker <brubakern@vmware.com>
2020-04-20 17:53:56 -0400

    Add support for downloading CSI volume objects
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- pkg/apis/velero/v1/download_request.go
+++ pkg/apis/velero/v1/download_request.go
@@ -29,12 +29,14 @@ type DownloadRequestSpec struct {
 type DownloadTargetKind string
 
 const (
-	DownloadTargetKindBackupLog             DownloadTargetKind = "BackupLog"
-	DownloadTargetKindBackupContents        DownloadTargetKind = "BackupContents"
-	DownloadTargetKindBackupVolumeSnapshots DownloadTargetKind = "BackupVolumeSnapshots"
-	DownloadTargetKindBackupResourceList    DownloadTargetKind = "BackupResourceList"
-	DownloadTargetKindRestoreLog            DownloadTargetKind = "RestoreLog"
-	DownloadTargetKindRestoreResults        DownloadTargetKind = "RestoreResults"
+	DownloadTargetKindBackupLog                 DownloadTargetKind = "BackupLog"
+	DownloadTargetKindBackupContents            DownloadTargetKind = "BackupContents"
+	DownloadTargetKindBackupVolumeSnapshots     DownloadTargetKind = "BackupVolumeSnapshots"
+	DownloadTargetKindBackupResourceList        DownloadTargetKind = "BackupResourceList"
+	DownloadTargetKindRestoreLog                DownloadTargetKind = "RestoreLog"
+	DownloadTargetKindRestoreResults            DownloadTargetKind = "RestoreResults"
+	DownloadTargetKindCSIVolumeSnapshots        DownloadTargetKind = "CSIVolumeSnapshots"
+	DownloadTargetKindCSIVolumeSnapshotContents DownloadTargetKind = "CSIVolumeSnapshotContents"
 )
 
 // DownloadTarget is the specification for what kind of file to download, and the name of the
