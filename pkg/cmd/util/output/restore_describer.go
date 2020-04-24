2550a40fda93398f041e02f6012d74a1952b37bf
FNU Raghavendra Manjunath <raghavendra@redhat.com>
2020-04-24 11:27:01 -0400

    Update pkg/cmd/util/output/restore_describer.go
    Co-Authored-By: Ashish Amarnath <ashisham@vmware.com>
    Signed-off-by: Raghavendra M <raghavendra@redhat.com>

--- pkg/cmd/util/output/restore_describer.go
+++ pkg/cmd/util/output/restore_describer.go
@@ -65,7 +65,7 @@ func DescribeRestore(restore *v1.Restore, podVolumeRestores []v1.PodVolumeRestor
 		d.Printf("Namespaces:\n")
 		var s string
 		if len(restore.Spec.IncludedNamespaces) == 0 {
-			s = "All namespaces found in the backup"
+			s = "all namespaces found in the backup"
 		} else if len(restore.Spec.IncludedNamespaces) == 1 && restore.Spec.IncludedNamespaces[0] == "*" {
 			s = "All namespaces found in the backup"
 		} else {
