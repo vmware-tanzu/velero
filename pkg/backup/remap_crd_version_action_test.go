2a053a0f888953d2d985eaf5f52b78abe049cc62
Nolan Brubaker <brubakern@vmware.com>
2020-04-29 15:19:38 -0400

    Fix existing tests
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- pkg/backup/remap_crd_version_action_test.go
+++ pkg/backup/remap_crd_version_action_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextfakes "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 
@@ -31,18 +32,31 @@ import (
 	velerotest "github.com/vmware-tanzu/velero/pkg/test"
 )
 
+// TODO: How do we get the elasticsearch & crossplane CRDs in here to run w/o a cluster?
 func TestRemapCRDVersionAction(t *testing.T) {
 	backup := &v1.Backup{}
-	a := NewRemapCRDVersionAction(velerotest.NewLogger())
+	clientset := apiextfakes.NewSimpleClientset()
+	betaClient := clientset.ApiextensionsV1beta1().CustomResourceDefinitions()
+
+	// build a v1beta1 CRD with the same name and add it to the fake client that the plugin is going to call.
+	// keep the same one for all 3 tests, since there's little value in recreating it
+	b := builder.ForCustomResourceDefinition("test.velero.io")
+	c := b.Result()
+	_, err := betaClient.Create(c)
+	require.NoError(t, err)
+
+	a := NewRemapCRDVersionAction(velerotest.NewLogger(), betaClient)
 
 	t.Run("Test a v1 CRD without any Schema information", func(t *testing.T) {
 		b := builder.ForV1CustomResourceDefinition("test.velero.io")
 		// Set a version that does not include and schema information.
 		b.Version(builder.ForV1CustomResourceDefinitionVersion("v1").Served(true).Storage(true).Result())
 		c := b.Result()
+
 		obj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&c)
 		require.NoError(t, err)
 
+		// Execute the plugin, which will call the fake client
 		item, _, err := a.Execute(&unstructured.Unstructured{Object: obj}, backup)
 		require.NoError(t, err)
 		assert.Equal(t, "apiextensions.k8s.io/v1beta1", item.UnstructuredContent()["apiVersion"])
