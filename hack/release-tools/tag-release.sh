37005fc4b8deac8b48f36e2a788c33141c89e411
Nolan Brubaker <brubakern@vmware.com>
2020-05-30 23:28:46 -0400

    Add copyright statement
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- hack/release-tools/tag-release.sh
+++ hack/release-tools/tag-release.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Copyright 2020 the Velero contributors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # This script will do the necessary checks and actions to create a release of Velero.
 # It will first validate that all prerequisites are met, then verify the version string is what the user expects.
 # A git tag will be created and pushed to GitHub, and GoReleaser will be invoked.
