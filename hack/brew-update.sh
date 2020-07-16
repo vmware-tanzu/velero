2376b697afc04a9abd85d19b58bc4578d793727c
Nolan Brubaker <brubakern@vmware.com>
2020-07-16 18:49:06 -0400

    Fix brew-update script, their commands changed (#2711)
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- hack/brew-update.sh
+++ hack/brew-update.sh
@@ -18,11 +18,5 @@ URL=https://github.com/vmware-tanzu/velero/archive/$VELERO_VERSION.tar.gz
 # Update brew so we're sure we have the latest Velero formula
 brew update
 
-# Download the Velero source tarball
-curl $URL --output velero-$VELERO_VERSION.tar.gz
-
-# Calculate the SHA 256 of the source tarball, since this is needed to bump the formula.
-SHA256=$(shasum -a 256 velero-$VELERO_VERSION.tar.gz)
-
 # Invoke brew's helper function, which will run all their tests and end up opening a browser with the resulting PR.
-brew bump-formula-pr --url $URL --sha256 $SHA256
+brew bump-formula-pr velero --url=$URL
