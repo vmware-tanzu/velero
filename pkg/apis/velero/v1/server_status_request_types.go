3898ed446b5419bdc4a91e10363c6dfbf808e440
Ashish Amarnath <ashisham@vmware.com>
2020-09-10 12:28:17 -0700

    🏃‍♂️ add shortnames for CRDs (#2911)
    Signed-off-by: Ashish Amarnath <ashisham@vmware.com>

--- pkg/apis/velero/v1/server_status_request_types.go
+++ pkg/apis/velero/v1/server_status_request_types.go
@@ -25,6 +25,7 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:shortName=ssr
 // +kubebuilder:object:generate=true
 // +kubebuilder:storageversion
 // +kubebuilder:subresource:status
