9a371607c219b170eddb315215e9782a24dfd111
Nolan Brubaker <brubakern@vmware.com>
2020-10-13 19:39:52 -0400

    Update test for lower case uid field
    Signed-off-by: Nolan Brubaker <brubakern@vmware.com>

--- pkg/restore/pv_restorer_test.go
+++ pkg/restore/pv_restorer_test.go
@@ -58,7 +58,7 @@ func TestExecutePVAction_NoSnapshotRestores(t *testing.T) {
 		},
 		{
 			name:        "ensure spec.claimRef UID is deleted",
-			obj:         NewTestUnstructured().WithName("pv-1").WithAnnotations("a", "b").WithSpec("someOtherField").WithSpecField("claimRef", map[string]interface{}{"UID": "a"}).Unstructured,
+			obj:         NewTestUnstructured().WithName("pv-1").WithAnnotations("a", "b").WithSpec("someOtherField").WithSpecField("claimRef", map[string]interface{}{"uid": "a"}).Unstructured,
 			restore:     builder.ForRestore(api.DefaultNamespace, "").RestorePVs(false).Result(),
 			backup:      defaultBackup().Phase(api.BackupPhaseInProgress).Result(),
 			expectedRes: NewTestUnstructured().WithAnnotations("a", "b").WithName("pv-1").WithSpec("someOtherField").WithSpecField("claimRef", map[string]interface{}{}).Unstructured,
