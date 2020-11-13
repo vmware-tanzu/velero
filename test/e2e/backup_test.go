b2468ddaea505958647c52a063e0c4fcb1c4f63a
Ashish Amarnath <ashisham@vmware.com>
2020-11-12 16:09:20 -0800

    Save
    Signed-off-by: Ashish Amarnath <ashisham@vmware.com>

--- test/e2e/backup_test.go
+++ test/e2e/backup_test.go
@@ -13,20 +13,11 @@ import (
 )
 
 var (
-	veleroCLI  string
-	namespace  string
-	backupName string
+	backupName  string
 	restoreName string
-	cloudPlatform string // aws, vsphere, azure
 )
 
-func init() {
-	flag.StringVar(&veleroCLI, "velerocli", "velero", "path to the velero application to use")
-	flag.StringVar(&namespace, "kibishiins", "kibishii", "namespace to use for Kibishii distributed data generator")
-	flag.StringVar(&cloudPlatform, "cloudplatform", "aws", "cloud platform we are deploying on (aws, vsphere, azure)")
-}
-
-var _ = Describe("Backup", func() {
+var _ = Describe("Backup_aws", func() {
 
 	BeforeEach(func() {
 		flag.Parse()
@@ -36,44 +27,44 @@ var _ = Describe("Backup", func() {
 			It("generates data, backups up the namespace, deletes the namespace, restores the namespace and verifies data", func() {
 				backupUUID, err := uuid.NewRandom()
 				Expect(err).NotTo(HaveOccurred())
-				backupName = "backup-"+backupUUID.String()
-				restoreName = "restore-"+backupUUID.String()
+				backupName = "backup-" + backupUUID.String()
+				restoreName = "restore-" + backupUUID.String()
 				println("backupName = " + backupName)
-				println("creating namespace " + namespace)
+				println("creating namespace " + kibishiNamespace)
 				timeoutCTX, _ := context.WithTimeout(context.Background(), time.Minute)
-				err = CreateNamespace(timeoutCTX, namespace)
+				err = CreateNamespace(timeoutCTX, kibishiNamespace)
 				Expect(err).NotTo(HaveOccurred())
 
-				println("installing kibishii in namespace " + namespace)
+				println("installing kibishii in namespace " + kibishiNamespace)
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute)
-				err = InstallKibishii(timeoutCTX, namespace, cloudPlatform)
+				err = InstallKibishii(timeoutCTX, kibishiNamespace, cloudPlatform)
 				Expect(err).NotTo(HaveOccurred())
 
 				println("running kibishii generate")
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute*60)
 
-				err = GenerateData(timeoutCTX, namespace, 2, 10, 10, 1024, 1024, 0, 2)
+				err = GenerateData(timeoutCTX, kibishiNamespace, 2, 10, 10, 1024, 1024, 0, 2)
 				Expect(err).NotTo(HaveOccurred())
 
 				println("executing backup")
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute*30)
 				/*
-				backupCmd := exec.CommandContext(timeoutCTX, veleroCLI, "create", "backup", backupName, "--include-namespaces", namespace,
-					"--default-volumes-to-restic", "--wait")
-				fmt.Printf("backup cmd =%v\n", backupCmd)
-				err = backupCmd.Run()
+					backupCmd := exec.CommandContext(timeoutCTX, veleroCLI, "create", "backup", backupName, "--include-namespaces", namespace,
+						"--default-volumes-to-restic", "--wait")
+					fmt.Printf("backup cmd =%v\n", backupCmd)
+					err = backupCmd.Run()
 				*/
 
-				err = BackupNamespace(timeoutCTX, veleroCLI, backupName, namespace)
+				err = BackupNamespace(timeoutCTX, veleroCLI, backupName, kibishiNamespace)
 				Expect(err).NotTo(HaveOccurred())
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute)
 				err = CheckBackupPhase(timeoutCTX, veleroCLI, backupName, velerov1.BackupPhaseCompleted)
 
 				Expect(err).NotTo(HaveOccurred())
 
-				println("removing namespace " + namespace)
+				println("removing namespace " + kibishiNamespace)
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute)
-				err = RemoveNamespace(timeoutCTX, namespace)
+				err = RemoveNamespace(timeoutCTX, kibishiNamespace)
 				Expect(err).NotTo(HaveOccurred())
 
 				println("restoring namespace")
@@ -85,12 +76,12 @@ var _ = Describe("Backup", func() {
 				println("running kibishii verify")
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute*60)
 
-				err = VerifyData(timeoutCTX, namespace, 2, 10, 10, 1024, 1024, 0, 2)
+				err = VerifyData(timeoutCTX, kibishiNamespace, 2, 10, 10, 1024, 1024, 0, 2)
 				Expect(err).NotTo(HaveOccurred())
 
-				println("removing namespace " + namespace)
+				println("removing namespace " + kibishiNamespace)
 				timeoutCTX, _ = context.WithTimeout(context.Background(), time.Minute)
-				err = RemoveNamespace(timeoutCTX, namespace)
+				err = RemoveNamespace(timeoutCTX, kibishiNamespace)
 				Expect(err).NotTo(HaveOccurred())
 			})
 		})
