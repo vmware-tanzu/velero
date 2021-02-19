45d53178ae4bc4e6809e862350e1477964ef70ae
David L. Smith-Uchida <dsmithuchida@vmware.com>
2021-02-18 16:16:59 -0800

    E2E tests now run in multiple clouds in addition to KIND (#3286)
    Split plug-in provider into cloud provider/object provider
    Moved velero install/uninstall for tests into velero_utils
    Added remove of CRDs to test v elero uninstall
    Added remove of cluster role binding to test velero uninstall
    Added dump of velero describe and logs on error
    Added velero namespace argument to velero_utils functions
    Modified api group versions e2e tests to use VeleroInstall
    Added velero logs dumps for api group versions e2e testing
    Added DeleteNamespace to test/e2e/common.go
    Fixed VeleroInstall to use the image specified
    Changed enable_api_group_versions_test to use veleroNamespace instead of hardcoded "velero"
    
    Signed-off-by: Dave Smith-Uchida <dsmithuchida@vmware.com>

--- test/e2e/common.go
+++ test/e2e/common.go
@@ -1,10 +1,15 @@
 package e2e
 
 import (
+	"fmt"
 	"os/exec"
+	"time"
+
+	"k8s.io/apimachinery/pkg/util/wait"
 
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -19,20 +24,26 @@ func EnsureClusterExists(ctx context.Context) error {
 }
 
 // GetClusterClient instantiates and returns a client for the cluster.
-func GetClusterClient() (*kubernetes.Clientset, error) {
+func GetClusterClient() (*kubernetes.Clientset, *apiextensionsclientset.Clientset, error) {
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	configOverrides := &clientcmd.ConfigOverrides{}
 	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
 	clientConfig, err := kubeConfig.ClientConfig()
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, nil, errors.WithStack(err)
 	}
 
 	client, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, nil, errors.WithStack(err)
 	}
-	return client, nil
+
+	extensionClientSet, err := apiextensionsclientset.NewForConfig(clientConfig)
+	if err != nil {
+		return nil, nil, errors.WithStack(err)
+	}
+
+	return client, extensionClientSet, nil
 }
 
 // CreateNamespace creates a kubernetes namespace
@@ -43,4 +54,19 @@ func CreateNamespace(ctx context.Context, client *kubernetes.Clientset, namespac
 		return nil
 	}
 	return err
+}
+
+func DeleteNamespace(ctx context.Context, client *kubernetes.Clientset, namespace string) error {
+	err := client.CoreV1().Namespaces().Delete(ctx, namespace, metav1.DeleteOptions{})
+	if err != nil {
+		return errors.WithMessagef(err, "Delete namespace failed removing namespace %s", namespace)
+	}
+	return wait.Poll(1*time.Second, 3*time.Minute, func() (bool, error) {
+		_, err := client.CoreV1().Namespaces().Get(ctx, namespace, metav1.GetOptions{})
+		if err != nil {
+			fmt.Printf("Namespaces.Get after delete return err %v\n", err)
+			return true, nil // Assume any error means the delete was successful
+		}
+		return false, nil
+	})
 }
--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -9,11 +9,13 @@ import (
 )
 
 var (
-	veleroCLI, veleroImage, cloudCredentialsFile, bslConfig, bslBucket, bslPrefix, vslConfig, pluginProvider string
+	veleroCLI, veleroImage, cloudCredentialsFile, bslConfig, bslBucket, bslPrefix, vslConfig, cloudProvider, objectStoreProvider, veleroNamespace string
+	installVelero, useVolumeSnapshots                                                                                                             bool
 )
 
 func init() {
-	flag.StringVar(&pluginProvider, "plugin-provider", "", "Provider of object store and volume snapshotter plugins. Required.")
+	flag.StringVar(&cloudProvider, "cloud-provider", "", "Cloud that Velero will be installed into.  Required.")
+	flag.StringVar(&objectStoreProvider, "object-store-provider", "", "Provider of object store plugin. Required if cloud-provider is kind, otherwise ignored.")
 	flag.StringVar(&bslBucket, "bucket", "", "name of the object storage bucket where backups from e2e tests should be stored. Required.")
 	flag.StringVar(&cloudCredentialsFile, "credentials-file", "", "file containing credentials for backup and volume provider. Required.")
 	flag.StringVar(&veleroCLI, "velerocli", "velero", "path to the velero application to use.")
@@ -21,6 +23,9 @@ func init() {
 	flag.StringVar(&bslConfig, "bsl-config", "", "configuration to use for the backup storage location. Format is key1=value1,key2=value2")
 	flag.StringVar(&bslPrefix, "prefix", "", "prefix under which all Velero data should be stored within the bucket. Optional.")
 	flag.StringVar(&vslConfig, "vsl-config", "", "configuration to use for the volume snapshot location. Format is key1=value1,key2=value2")
+	flag.StringVar(&veleroNamespace, "velero-namespace", "velero", "Namespace to install Velero into")
+	flag.BoolVar(&installVelero, "install-velero", true, "Install/uninstall velero during the test.  Optional.")
+	flag.BoolVar(&useVolumeSnapshots, "use-volume-snapshots", false, "Use volume-snapshotter plugin for volume backup.  Optional")
 }
 
 func TestE2e(t *testing.T) {
