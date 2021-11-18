e09837cc4c7d36740f05b7dd3435f88534b27208
danfengl <danfengl@vmware.com>
2021-11-18 08:50:50 +0000

    Fix snapshot e2e test issue of jsonpath
    Signed-off-by: danfengl <danfengl@vmware.com>

--- test/e2e/velero_utils.go
+++ test/e2e/velero_utils.go
@@ -422,7 +422,7 @@ func veleroAddPluginsForProvider(ctx context.Context, veleroCLI string, veleroNa
 func waitForVSphereUploadCompletion(ctx context.Context, timeout time.Duration, namespace string) error {
 	err := wait.PollImmediate(time.Minute, timeout, func() (bool, error) {
 		checkSnapshotCmd := exec.CommandContext(ctx, "kubectl",
-			"get", "-n", namespace, "snapshots.backupdriver.cnsdp.vmware.com", "-o=jsonpath='{range .items[*]}{.spec.resourceHandle.name}{\"=\"}{.status.phase}{\"\\n\"}'")
+			"get", "-n", namespace, "snapshots.backupdriver.cnsdp.vmware.com", "-o=jsonpath='{range .items[*]}{.spec.resourceHandle.name}{\"=\"}{.status.phase}{\"\\n\"}{end}'")
 		fmt.Printf("checkSnapshotCmd cmd =%v\n", checkSnapshotCmd)
 		stdout, stderr, err := veleroexec.RunCommand(checkSnapshotCmd)
 		if err != nil {
