b8b5427388b0877055a4238073c69c84e270b0c5
Hoang, Phuong <phuong.n.hoang@dell.com>
2022-03-15 19:54:24 -0400

    Fix format.
    Signed-off-by: Hoang, Phuong <phuong.n.hoang@dell.com>

--- pkg/restore/pod_action_test.go
+++ pkg/restore/pod_action_test.go
@@ -17,8 +17,6 @@ limitations under the License.
 package restore
 
 import (
-	"github.com/vmware-tanzu/velero/pkg/kuberesource"
-	//"github.com/vmware-tanzu/velero/pkg/kuberesource"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,6 +26,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 
+	"github.com/vmware-tanzu/velero/pkg/kuberesource"
 	"github.com/vmware-tanzu/velero/pkg/plugin/velero"
 	velerotest "github.com/vmware-tanzu/velero/pkg/test"
 )
