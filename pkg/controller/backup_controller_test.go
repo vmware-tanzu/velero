9027e0b72832963f8f6a95036551dbed74536943
Xun Jiang <blackpiglet@gmail.com>
2022-10-28 23:43:53 +0800

    Fix v1.9.3 CSI VolumeSnapshot status duplicate issue.
    Signed-off-by: Xun Jiang <blackpiglet@gmail.com>

--- pkg/controller/backup_controller_test.go
+++ pkg/controller/backup_controller_test.go
@@ -27,6 +27,9 @@ import (
 	"testing"
 	"time"
 
+	snapshotv1api "github.com/kubernetes-csi/external-snapshotter/client/v4/apis/volumesnapshot/v1"
+	snapshotfake "github.com/kubernetes-csi/external-snapshotter/client/v4/clientset/versioned/fake"
+	snapshotinformers "github.com/kubernetes-csi/external-snapshotter/client/v4/informers/externalversions"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
@@ -1160,6 +1163,104 @@ func Test_getLastSuccessBySchedule(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			assert.Equal(t, tc.want, getLastSuccessBySchedule(tc.backups))
+		})
+	}
+}
+
+func TestDeleteVolumeSnapshot(t *testing.T) {
+	tests := []struct {
+		name             string
+		vsArray          []snapshotv1api.VolumeSnapshot
+		vscArray         []*snapshotv1api.VolumeSnapshotContent
+		expectedVSArray  []snapshotv1api.VolumeSnapshot
+		expectedVSCArray []snapshotv1api.VolumeSnapshotContent
+	}{
+		{
+			name: "VS is ReadyToUse, and VS has corresponding VSC. VS should be deleted.",
+			vsArray: []snapshotv1api.VolumeSnapshot{
+				*builder.ForVolumeSnapshot("velero", "vs1").ObjectMeta(builder.WithLabels("testing-vs", "vs1")).Status().BoundVolumeSnapshotContentName("vsc1").Result(),
+			},
+			vscArray: []*snapshotv1api.VolumeSnapshotContent{
+				builder.ForVolumeSnapshotContent("vsc1").DeletionPolicy(snapshotv1api.VolumeSnapshotContentDelete).Status().Result(),
+			},
+			expectedVSArray: []snapshotv1api.VolumeSnapshot{},
+			expectedVSCArray: []snapshotv1api.VolumeSnapshotContent{
+				*builder.ForVolumeSnapshotContent("vsc1").DeletionPolicy(snapshotv1api.VolumeSnapshotContentRetain).VolumeSnapshotRef("ns-", "name-").Status().Result(),
+			},
+		},
+		{
+			name: "Corresponding VSC not found for VS. VS is not deleted.",
+			vsArray: []snapshotv1api.VolumeSnapshot{
+				*builder.ForVolumeSnapshot("velero", "vs1").ObjectMeta(builder.WithLabels("testing-vs", "vs1")).Status().BoundVolumeSnapshotContentName("vsc1").Result(),
+			},
+			vscArray: []*snapshotv1api.VolumeSnapshotContent{},
+			expectedVSArray: []snapshotv1api.VolumeSnapshot{
+				*builder.ForVolumeSnapshot("velero", "vs1").Status().BoundVolumeSnapshotContentName("vsc1").Result(),
+			},
+			expectedVSCArray: []snapshotv1api.VolumeSnapshotContent{},
+		},
+		{
+			name: "VS status is nil. VSC should not be modified.",
+			vsArray: []snapshotv1api.VolumeSnapshot{
+				*builder.ForVolumeSnapshot("velero", "vs1").ObjectMeta(builder.WithLabels("testing-vs", "vs1")).Result(),
+			},
+			vscArray: []*snapshotv1api.VolumeSnapshotContent{
+				builder.ForVolumeSnapshotContent("vsc1").DeletionPolicy(snapshotv1api.VolumeSnapshotContentDelete).Status().Result(),
+			},
+			expectedVSArray: []snapshotv1api.VolumeSnapshot{},
+			expectedVSCArray: []snapshotv1api.VolumeSnapshotContent{
+				*builder.ForVolumeSnapshotContent("vsc1").DeletionPolicy(snapshotv1api.VolumeSnapshotContentDelete).Status().Result(),
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			fakeClient := velerotest.NewFakeControllerRuntimeClientBuilder(t).WithLists().Build()
+
+			vsClient := snapshotfake.NewSimpleClientset()
+			for _, vsc := range tc.vscArray {
+				_, err := vsClient.SnapshotV1().VolumeSnapshotContents().Create(context.TODO(), vsc, metav1.CreateOptions{})
+				require.NoError(t, err)
+				err = fakeClient.Create(context.TODO(), vsc, &kbclient.CreateOptions{})
+				require.NoError(t, err)
+			}
+			for _, vs := range tc.vsArray {
+				_, err := vsClient.SnapshotV1().VolumeSnapshots("velero").Create(context.TODO(), &vs, metav1.CreateOptions{})
+				require.NoError(t, err)
+				err = fakeClient.Create(context.TODO(), &vs, &kbclient.CreateOptions{})
+				require.NoError(t, err)
+			}
+
+			sharedInformers := snapshotinformers.NewSharedInformerFactory(vsClient, 0)
+
+			for _, vs := range tc.vsArray {
+				sharedInformers.Snapshot().V1().VolumeSnapshots().Informer().GetStore().Add(vs)
+			}
+
+			logger := logging.DefaultLogger(logrus.DebugLevel, logging.FormatText)
+			c := &backupController{
+				kbClient:             fakeClient,
+				volumeSnapshotClient: vsClient,
+				volumeSnapshotLister: sharedInformers.Snapshot().V1().VolumeSnapshots().Lister(),
+			}
+
+			c.deleteVolumeSnapshot(tc.vsArray, tc.vscArray, logger)
+
+			vsList, err := c.volumeSnapshotClient.SnapshotV1().VolumeSnapshots("velero").List(context.TODO(), metav1.ListOptions{})
+			require.NoError(t, err)
+			assert.Equal(t, len(tc.expectedVSArray), len(vsList.Items))
+			for index := range tc.expectedVSArray {
+				assert.Equal(t, tc.expectedVSArray[index].Status, vsList.Items[index].Status)
+				assert.Equal(t, tc.expectedVSArray[index].Spec, vsList.Items[index].Spec)
+			}
+
+			vscList, err := c.volumeSnapshotClient.SnapshotV1().VolumeSnapshotContents().List(context.TODO(), metav1.ListOptions{})
+			require.NoError(t, err)
+			assert.Equal(t, len(tc.expectedVSCArray), len(vscList.Items))
+			for index := range tc.expectedVSCArray {
+				assert.Equal(t, tc.expectedVSCArray[index].Spec, vscList.Items[index].Spec)
+			}
 		})
 	}
 }
