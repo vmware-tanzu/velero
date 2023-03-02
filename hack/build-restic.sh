3c671a7c09b7caa4bd729d557aa6cb63ef397805
DingRui Yang <abserari@gmail.com>
2023-03-02 21:11:45 +0800

    Fix miss ${GOARM}" parameter in build-restic.sh
    Signed-off-by: DingRui Yang <abserari@gmail.com>

--- hack/build-restic.sh
+++ hack/build-restic.sh
@@ -50,6 +50,10 @@ fi
 mkdir ${build_path}/restic
 git clone -b v${RESTIC_VERSION} https://github.com/restic/restic.git ${build_path}/restic
 pushd ${build_path}/restic
-go run build.go --goos "${GOOS}" --goarch "${GOARCH}" --goarm "${GOARM}" -o ${restic_bin}
+if [ -z "${GOARM}" ]; then
+    go run build.go --goos "${GOOS}" --goarch "${GOARCH}" -o ${restic_bin}
+else
+    go run build.go --goos "${GOOS}" --goarch "${GOARCH}" --goarm "${GOARM}" -o ${restic_bin}
+fi
 chmod +x ${restic_bin}
 popd
