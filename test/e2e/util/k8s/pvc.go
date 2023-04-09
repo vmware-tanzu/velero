8628388445b7d1949a6ec3ccb76f0f4e42db7bfa
danfengl <danfengl@vmware.com>
2023-04-09 01:42:08 +0000

    [Cherry-pick 1.11]Add E2E test for schedule backup creation principle
    Signed-off-by: danfengl <danfengl@vmware.com>

--- test/e2e/util/k8s/pvc.go
+++ test/e2e/util/k8s/pvc.go
@@ -38,7 +38,7 @@ func CreatePVC(client TestClient, ns, name, sc string, ann map[string]string) (*
 			},
 			Resources: corev1.ResourceRequirements{
 				Requests: corev1.ResourceList{
-					corev1.ResourceStorage: resource.MustParse("1Gi"),
+					corev1.ResourceStorage: resource.MustParse("1Mi"),
 				},
 			},
 			StorageClassName: &sc,
