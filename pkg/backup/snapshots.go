f9e716a8c9df06feb779f1801436bdf1dd6b0649
Tiger Kaovilai <tkaovila@redhat.com>
2023-11-02 16:14:55 -0400

    skip this if SnapshotMoveData
    https://github.com/vmware-tanzu/velero/pull/7046/files#r1380708644
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/backup/snapshots.go
+++ pkg/backup/snapshots.go
@@ -16,49 +16,52 @@ import (
 
 // Common function to update the status of CSI snapshots
 // returns VolumeSnapshot, VolumeSnapshotContent, VolumeSnapshotClasses referenced
-func UpdateBackupCSISnapshotsStatus(client kbclient.Client, volumeSnapshotLister snapshotv1listers.VolumeSnapshotLister, backup *velerov1api.Backup, backupLog logrus.FieldLogger) ([]snapshotv1api.VolumeSnapshot, []snapshotv1api.VolumeSnapshotContent, []snapshotv1api.VolumeSnapshotClass) {
-	var volumeSnapshots []snapshotv1api.VolumeSnapshot
-	var volumeSnapshotContents []snapshotv1api.VolumeSnapshotContent
-	var volumeSnapshotClasses []snapshotv1api.VolumeSnapshotClass
-	selector := label.NewSelectorForBackup(backup.Name)
-	vscList := &snapshotv1api.VolumeSnapshotContentList{}
-
-	if volumeSnapshotLister != nil {
-		tmpVSs, err := volumeSnapshotLister.List(label.NewSelectorForBackup(backup.Name))
+func UpdateBackupCSISnapshotsStatus(client kbclient.Client, volumeSnapshotLister snapshotv1listers.VolumeSnapshotLister, backup *velerov1api.Backup, backupLog logrus.FieldLogger) (volumeSnapshots []snapshotv1api.VolumeSnapshot, volumeSnapshotContents []snapshotv1api.VolumeSnapshotContent, volumeSnapshotClasses []snapshotv1api.VolumeSnapshotClass) {
+	if boolptr.IsSetToTrue(backup.Spec.SnapshotMoveData) {
+		backupLog.Info("backup SnapshotMoveData is set to true, skip VolumeSnapshot resource persistence.")
+	} else {
+		selector := label.NewSelectorForBackup(backup.Name)
+		vscList := &snapshotv1api.VolumeSnapshotContentList{}
+	
+		if volumeSnapshotLister != nil {
+			tmpVSs, err := volumeSnapshotLister.List(label.NewSelectorForBackup(backup.Name))
+			if err != nil {
+				backupLog.Error(err)
+			}
+			for _, vs := range tmpVSs {
+				volumeSnapshots = append(volumeSnapshots, *vs)
+			}
+		}
+	
+		err := client.List(context.Background(), vscList, &kbclient.ListOptions{LabelSelector: selector})
 		if err != nil {
 			backupLog.Error(err)
 		}
-		for _, vs := range tmpVSs {
-			volumeSnapshots = append(volumeSnapshots, *vs)
+		if len(vscList.Items) >= 0 {
+			volumeSnapshotContents = vscList.Items
 		}
-	}
-
-	err := client.List(context.Background(), vscList, &kbclient.ListOptions{LabelSelector: selector})
-	if err != nil {
-		backupLog.Error(err)
-	}
-	if len(vscList.Items) >= 0 {
-		volumeSnapshotContents = vscList.Items
-	}
-
-	vsClassSet := sets.NewString()
-	for index := range volumeSnapshotContents {
-		// persist the volumesnapshotclasses referenced by vsc
-		if volumeSnapshotContents[index].Spec.VolumeSnapshotClassName != nil && !vsClassSet.Has(*volumeSnapshotContents[index].Spec.VolumeSnapshotClassName) {
-			vsClass := &snapshotv1api.VolumeSnapshotClass{}
-			if err := client.Get(context.TODO(), kbclient.ObjectKey{Name: *volumeSnapshotContents[index].Spec.VolumeSnapshotClassName}, vsClass); err != nil {
-				backupLog.Error(err)
-			} else {
-				vsClassSet.Insert(*volumeSnapshotContents[index].Spec.VolumeSnapshotClassName)
-				volumeSnapshotClasses = append(volumeSnapshotClasses, *vsClass)
+	
+		vsClassSet := sets.NewString()
+		for index := range volumeSnapshotContents {
+			// persist the volumesnapshotclasses referenced by vsc
+			if volumeSnapshotContents[index].Spec.VolumeSnapshotClassName != nil && !vsClassSet.Has(*volumeSnapshotContents[index].Spec.VolumeSnapshotClassName) {
+				vsClass := &snapshotv1api.VolumeSnapshotClass{}
+				if err := client.Get(context.TODO(), kbclient.ObjectKey{Name: *volumeSnapshotContents[index].Spec.VolumeSnapshotClassName}, vsClass); err != nil {
+					backupLog.Error(err)
+				} else {
+					vsClassSet.Insert(*volumeSnapshotContents[index].Spec.VolumeSnapshotClassName)
+					volumeSnapshotClasses = append(volumeSnapshotClasses, *vsClass)
+				}
 			}
 		}
-	}
-	backup.Status.CSIVolumeSnapshotsAttempted = len(volumeSnapshots)
-	for _, vs := range volumeSnapshots {
-		if vs.Status != nil && boolptr.IsSetToTrue(vs.Status.ReadyToUse) {
-			backup.Status.CSIVolumeSnapshotsCompleted++
+		backup.Status.CSIVolumeSnapshotsAttempted = len(volumeSnapshots)
+		csiVolumeSnapshotsCompleted := 0
+		for _, vs := range volumeSnapshots {
+			if vs.Status != nil && boolptr.IsSetToTrue(vs.Status.ReadyToUse) {
+				csiVolumeSnapshotsCompleted++
+			}
 		}
+		backup.Status.CSIVolumeSnapshotsCompleted = csiVolumeSnapshotsCompleted
 	}
 	return volumeSnapshots, volumeSnapshotContents, volumeSnapshotClasses
 }
