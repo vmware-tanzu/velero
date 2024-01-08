0722927df25b20dd1bf7860cd09aa0b32efd339a
Lyndon-Li <lyonghui@vmware.com>
2024-01-08 10:10:51 +0800

    isue 6928: remove snapshot deletion timeout for PVB
    Signed-off-by: Lyndon-Li <lyonghui@vmware.com>

--- pkg/controller/backup_deletion_controller.go
+++ pkg/controller/backup_deletion_controller.go
@@ -59,7 +59,6 @@ import (
 )
 
 const (
-	snapshotDeleteTimeout     = time.Minute
 	deleteBackupRequestMaxAge = 24 * time.Hour
 )
 
@@ -513,12 +512,9 @@ func (r *backupDeletionReconciler) deletePodVolumeSnapshots(ctx context.Context,
 		return []error{err}
 	}
 
-	ctx2, cancelFunc := context.WithTimeout(ctx, snapshotDeleteTimeout)
-	defer cancelFunc()
-
 	var errs []error
 	for _, snapshot := range snapshots {
-		if err := r.repoMgr.Forget(ctx2, snapshot); err != nil {
+		if err := r.repoMgr.Forget(ctx, snapshot); err != nil {
 			errs = append(errs, err)
 		}
 	}
