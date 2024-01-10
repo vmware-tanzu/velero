1cc6f743418c2b4959f7c0838ba6afedd69fb597
Wenkai Yin(尹文开) <yinw@vmware.com>
2024-01-10 09:45:57 +0800

    Make "disable-informer-cache" option false(enabled) by default to keep it consistent with the help message
    Make "disable-informer-cache" option false(enabled) by default to keep it consi
    stent with the help message
    
    Fixes #7264
    
    Signed-off-by: Wenkai Yin(尹文开) <yinw@vmware.com>

--- pkg/cmd/cli/install/install.go
+++ pkg/cmd/cli/install/install.go
@@ -151,7 +151,7 @@ func NewInstallOptions() *Options {
 		DefaultVolumesToFsBackup: false,
 		UploaderType:             uploader.KopiaType,
 		DefaultSnapshotMoveData:  false,
-		DisableInformerCache:     true,
+		DisableInformerCache:     false,
 	}
 }
 
