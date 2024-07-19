82faa554bda815fef10bdad4e6d7004f53653ce9
Anshul Ahuja <anshul.ahu@gmail.com>
2024-07-19 04:32:39 +0000

    Fail Delete Backup if BSL is not available
    Signed-off-by: Anshul Ahuja <anshul.ahu@gmail.com>

--- pkg/controller/backup_deletion_controller_test.go
+++ pkg/controller/backup_deletion_controller_test.go
@@ -125,8 +125,13 @@ func TestBackupDeletionControllerReconcile(t *testing.T) {
 		td := setupBackupDeletionControllerTest(t, defaultTestDbr(), location, backup)
 		td.controller.backupStoreGetter = &fakeErrorBackupStoreGetter{}
 		_, err := td.controller.Reconcile(ctx, td.req)
-		assert.Error(t, err)
-		assert.True(t, strings.HasPrefix(err.Error(), "error getting the backup store"))
+		assert.Nil(t, err)
+		res := &velerov1api.DeleteBackupRequest{}
+		err = td.fakeClient.Get(ctx, td.req.NamespacedName, res)
+		require.NoError(t, err)
+		assert.Equal(t, "Processed", string(res.Status.Phase))
+		assert.Equal(t, 1, len(res.Status.Errors))
+		assert.True(t, strings.HasPrefix(res.Status.Errors[0], fmt.Sprintf("cannot delete backup because backup storage location %s is currently unavailable", location.Name)))
 	})
 
 	t.Run("missing spec.backupName", func(t *testing.T) {
