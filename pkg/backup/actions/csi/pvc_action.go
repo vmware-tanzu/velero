d069d0acc772599022753b07c4e9fa4e6c704d16
Carlo Field <carlo.field@adfinis.com>
2025-01-23 17:49:32 +0100

    feat(csi): allow setting annotation on StorageClass to determine VolumeSnapshotClass
    Signed-off-by: Carlo Field <carlo.field@adfinis.com>

--- pkg/backup/actions/csi/pvc_action.go
+++ pkg/backup/actions/csi/pvc_action.go
@@ -161,7 +161,7 @@ func (p *pvcBackupItemAction) createVolumeSnapshot(
 
 	p.log.Debugf("Fetching VolumeSnapshotClass for %s", storageClass.Provisioner)
 	vsClass, err := csi.GetVolumeSnapshotClass(
-		storageClass.Provisioner,
+		storageClass,
 		backup,
 		&pvc,
 		p.log,
