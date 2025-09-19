69684671a66b5136dd3aa91bd148bfd2d13cf030
Joseph <jvaikath@redhat.com>
2025-09-19 15:50:55 -0400

    Update backup status with expansion

--- pkg/util/collections/includes_excludes.go
+++ pkg/util/collections/includes_excludes.go
@@ -143,19 +143,21 @@ func (nie *NamespaceIncludesExcludes) IncludeEverything() bool {
 }
 
 // ResolveNamespaceList returns a list of all namespaces which will be backed up.
-func (nie *NamespaceIncludesExcludes) ResolveNamespaceList() ([]string, error) {
+func (nie *NamespaceIncludesExcludes) ResolveNamespaceList() ([]string, bool,error) {
 	includes := nie.GetIncludes()
 	excludes := nie.GetExcludes()
+	wildcardExpansion := false
 
 	// Check if we should use wildcard expansion
 	if wildcard.ShouldExpandWildcards(includes, excludes) {
+		wildcardExpansion = true
 		expandedIncludes, expandedExcludes, err := wildcard.ExpandWildcards(
 			nie.activeNamespaces, includes, excludes)
 		if err != nil {
 			// Log error and fall back to traditional logic
 			logrus.WithError(err).Error("Error expanding wildcard patterns in namespace includes/excludes")
 			// (or return error - needs decision)
-			return nil, err
+			return nil, wildcardExpansion, err
 		}
 		// Set the includes and excludes
 		nie.SetIncludes(expandedIncludes)
@@ -163,10 +165,10 @@ func (nie *NamespaceIncludesExcludes) ResolveNamespaceList() ([]string, error) {
 
 	}
 
-	// This logic is called regardless of whether wildcard expansion is needed, 
+	// This logic is called regardless of whether wildcard expansion is needed,
 	// because we update the includes and excludes in the previous step
 	// if wildcard expansion has occured
-	return nie.resolveNamespaceListTraditional(), nil
+	return nie.resolveNamespaceListTraditional(), wildcardExpansion, nil
 }
 
 // resolveNamespaceListTraditional is a helper method to resolve namespaces using the old glob-based logic.
