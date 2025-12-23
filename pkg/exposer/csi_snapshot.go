21c2a472aa1886d24957ea2b577b5d8dcd3bc029
Lubron Zhan <lubronzhan@gmail.com>
2025-12-22 22:59:17 -0800

    Update the logging to print correct affinity field
    Signed-off-by: Lubron Zhan <lubronzhan@gmail.com>

--- pkg/exposer/csi_snapshot.go
+++ pkg/exposer/csi_snapshot.go
@@ -259,7 +259,13 @@ func (e *csiSnapshotExposer) Expose(ctx context.Context, ownerObject corev1api.O
 		return errors.Wrap(err, "error to create backup pod")
 	}
 
-	curLog.WithField("pod name", backupPod.Name).WithField("affinity", csiExposeParam.Affinity).Info("Backup pod is created")
+	curLog = curLog.WithField("pod name", backupPod.Name)
+	if affinity != nil {
+		curLog = curLog.WithField("affinity", *affinity)
+	} else {
+		curLog = curLog.WithField("affinity", "nil")
+	}
+	curLog.Info("Backup pod is created")
 
 	defer func() {
 		if err != nil {
