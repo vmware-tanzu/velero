5dd9d5242b9439917c2302184ea24b7086505742
Scott Seago <sseago@redhat.com>
2026-04-07 13:22:38 -0400

    Add custom action type to volume policies (#9540)
    * Add custom action type to volume policies
    
    Signed-off-by: Scott Seago <sseago@redhat.com>
    
    * Update internal/resourcepolicies/resource_policies.go
    
    Co-authored-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>
    Signed-off-by: Scott Seago <sseago@redhat.com>
    
    * added "custom" to validation list
    
    Signed-off-by: Scott Seago <sseago@redhat.com>
    
    * responding to review comments
    
    Signed-off-by: Scott Seago <sseago@redhat.com>
    
    ---------
    
    Signed-off-by: Scott Seago <sseago@redhat.com>
    Co-authored-by: Tiger Kaovilai <passawit.kaovilai@gmail.com>
    Signed-off-by: Scott Seago <sseago@redhat.com>

--- internal/resourcepolicies/volume_resources_validator.go
+++ internal/resourcepolicies/volume_resources_validator.go
@@ -90,7 +90,7 @@ func decodeStruct(r io.Reader, s any) error {
 func (a *Action) validate() error {
 	// validate Type
 	valid := false
-	if a.Type == Skip || a.Type == Snapshot || a.Type == FSBackup {
+	if a.Type == Skip || a.Type == Snapshot || a.Type == FSBackup || a.Type == Custom {
 		valid = true
 	}
 	if !valid {
