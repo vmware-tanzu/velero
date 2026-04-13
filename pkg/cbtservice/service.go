97ffb9eaaf2c6239ee898095b6966cacc41c17e1
Lyndon-Li <lyonghui@vmware.com>
2026-04-13 16:28:28 +0800

    add cbt interfaces
    Signed-off-by: Lyndon-Li <lyonghui@vmware.com>

---
+++ pkg/cbtservice/service.go
@@ -0,0 +1,34 @@
+/*
+Copyright The Velero Contributors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cbtservice
+
+import "context"
+
+// Range defines the range of a change
+type Range struct {
+	Offset int64
+	Length int64
+}
+
+// Service defines the methods for CBT service which could be implemented by Kubernetes SnapshotMetadataService or other customized services
+type Service interface {
+	// GetAllocatedBlocks enumerates the allocated blocks of the snapshot and call the record callback
+	GetAllocatedBlocks(ctx context.Context, snapshot string, record func([]Range) error) error
+
+	// GetChangedBlocks enumerates the changed blocks of the snapshot since PIT of changeID and call the record callback
+	GetChangedBlocks(ctx context.Context, snapshot string, changeID string, record func([]Range) error) error
+}
