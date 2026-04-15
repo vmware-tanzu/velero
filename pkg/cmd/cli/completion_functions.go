c6a86807966f90375b0286693a1e33bdc50e66df
Tiger Kaovilai <tkaovila@redhat.com>
2026-04-15 10:45:57 -0400

    Refactor completion function to use apimachinery's meta helpers and add unit tests for completion logic
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/cmd/cli/completion_functions.go
+++ pkg/cmd/cli/completion_functions.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/api/meta"
 	kbclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	velerov1api "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -31,10 +32,9 @@ import (
 // completionFunc is the function signature for cobra's ValidArgsFunction.
 type completionFunc = func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective)
 
-// completeNames is a generic helper that builds a completion function for any
-// Velero list type. The caller provides a constructor for the list object and a
-// callback that extracts resource names from it.
-func completeNames[L kbclient.ObjectList](f client.Factory, newList func() L, getNames func(L) []string) completionFunc {
+// completeNames builds a completion function for any Velero list type.
+// It extracts resource names via apimachinery's meta helpers.
+func completeNames(f client.Factory, list kbclient.ObjectList) completionFunc {
 	return func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		kbClient, err := f.KubebuilderClient()
 		if err != nil {
@@ -42,13 +42,21 @@ func completeNames[L kbclient.ObjectList](f client.Factory, newList func() L, ge
 		}
 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 		defer cancel()
-		list := newList()
-		if err := kbClient.List(ctx, list, &kbclient.ListOptions{Namespace: f.Namespace()}); err != nil {
+		freshList := list.DeepCopyObject().(kbclient.ObjectList)
+		if err := kbClient.List(ctx, freshList, &kbclient.ListOptions{Namespace: f.Namespace()}); err != nil {
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+		items, err := meta.ExtractList(freshList)
+		if err != nil {
 			return nil, cobra.ShellCompDirectiveNoFileComp
 		}
 		var filtered []string
-		for _, name := range getNames(list) {
-			if strings.HasPrefix(name, toComplete) {
+		for _, item := range items {
+			accessor, err := meta.Accessor(item)
+			if err != nil {
+				continue
+			}
+			if name := accessor.GetName(); strings.HasPrefix(name, toComplete) {
 				filtered = append(filtered, name)
 			}
 		}
@@ -57,79 +65,25 @@ func completeNames[L kbclient.ObjectList](f client.Factory, newList func() L, ge
 }
 
 func CompleteBackupNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.BackupList { return new(velerov1api.BackupList) },
-		func(l *velerov1api.BackupList) []string {
-			names := make([]string, len(l.Items))
-			for i, b := range l.Items {
-				names[i] = b.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.BackupList{})
 }
 
 func CompleteRestoreNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.RestoreList { return new(velerov1api.RestoreList) },
-		func(l *velerov1api.RestoreList) []string {
-			names := make([]string, len(l.Items))
-			for i, r := range l.Items {
-				names[i] = r.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.RestoreList{})
 }
 
 func CompleteScheduleNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.ScheduleList { return new(velerov1api.ScheduleList) },
-		func(l *velerov1api.ScheduleList) []string {
-			names := make([]string, len(l.Items))
-			for i, s := range l.Items {
-				names[i] = s.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.ScheduleList{})
 }
 
 func CompleteBackupStorageLocationNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.BackupStorageLocationList { return new(velerov1api.BackupStorageLocationList) },
-		func(l *velerov1api.BackupStorageLocationList) []string {
-			names := make([]string, len(l.Items))
-			for i, loc := range l.Items {
-				names[i] = loc.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.BackupStorageLocationList{})
 }
 
 func CompleteVolumeSnapshotLocationNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.VolumeSnapshotLocationList { return new(velerov1api.VolumeSnapshotLocationList) },
-		func(l *velerov1api.VolumeSnapshotLocationList) []string {
-			names := make([]string, len(l.Items))
-			for i, loc := range l.Items {
-				names[i] = loc.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.VolumeSnapshotLocationList{})
 }
 
 func CompleteBackupRepositoryNames(f client.Factory) completionFunc {
-	return completeNames(f,
-		func() *velerov1api.BackupRepositoryList { return new(velerov1api.BackupRepositoryList) },
-		func(l *velerov1api.BackupRepositoryList) []string {
-			names := make([]string, len(l.Items))
-			for i, r := range l.Items {
-				names[i] = r.Name
-			}
-			return names
-		},
-	)
+	return completeNames(f, &velerov1api.BackupRepositoryList{})
 }
